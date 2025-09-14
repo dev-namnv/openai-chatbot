d08b5d2d4add252dab63661e9dd53ac44225f1fc
NamNV <dev.namnv@gmail.com>
2025-09-15 04:27:52 +0700

    Fix CORS

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,7 @@ async function bootstrap(): Promise<void> {
   SwaggerModule.setup('api-docs', app, document);
 
   app.enableCors({
-    origin: [/^http:\/\/localhost:\d+$/, 'https://ai-chatbot-ccdd1.web.app'],
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: '*',
   });
